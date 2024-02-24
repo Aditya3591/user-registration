b6c304f29c57a8d1bcfba9bd55cc8b0edf0fc9cb
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-24 11:04:46 +0530

    [aditya] added: validate the last name

--- reg.py
+++ reg.py
@@ -1,19 +1,22 @@
 import re
 
-def validate_name(frist_name):
+def validate_name(name):
 
     pattern=r'[A-Z][a-z]{2,}'
 
-    if re.match(pattern,first_name):
+    if re.match(pattern,name):
         return True
     else:
         return False
     
 
 first_name=input("enter the first name :")
-if validate_name(first_name):
+last_name=input("enter the last name :")
 
-    print("valid frist name")
+
+if validate_name(first_name) and validate_name(last_name):
+
+    print("the name is valid")
 
 else:
     print("the name is not valid")
