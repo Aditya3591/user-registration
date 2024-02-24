20eada4f7a2ceee148bb543bb9f72206f39169c7
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-24 12:50:35 +0530

    [aditya] added: email validation

--- reg.py
+++ reg.py
@@ -9,6 +9,17 @@ def validate_name(name):
     else:
         return False
     
+def validate_email(email):
+
+
+    email_pattern=re.compile("[^0-9][a-z0-9\-\+]+[.a-z0-9]@[a-z0-9]+.[a-z]+[.in]")
+    email_valid=email_pattern.match(email)
+
+    if email_valid:
+        print("the email is valid")
+    else:
+        print("the email is not valid")
+
 
 first_name=input("enter the first name :")
 last_name=input("enter the last name :")
@@ -21,3 +32,5 @@ if validate_name(first_name) and validate_name(last_name):
 else:
     print("the name is not valid")
 
+email=input("enter the email :")
+validate_email(email)
